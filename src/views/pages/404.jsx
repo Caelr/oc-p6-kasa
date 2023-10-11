f7488437ce8629e89e7397e01a9ec35ce30a2096
Rachidi Judicaël <wahab@MacBook-Pro-de-Rachidi.local>
2023-10-11 21:59:02 +0400

    feat:404

--- src/views/pages/404.jsx
+++ src/views/pages/404.jsx
@@ -4,8 +4,8 @@ export default function Error() {
   return (
     <main className="error">
       <h1 className="error__title">404</h1>
-      <h2 className="error__description">Oups! La page que vous demandez n'existe pas.</h2>
-      <Link to='/' className="error__link active">Retourner sur la page d’accueil</Link>
+      <h2 className="error__description">Oups! La page que <br className="mobile__br" /> vous demandez n'existe pas.</h2>
+      <Link to='/' className="error__link">Retourner sur la page d’accueil</Link>
     </main>
   )
 }
