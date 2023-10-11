f15dc2f05a94ab2be4cb1dbfb8bb514f0b47cb88
Rachidi Judicaël <wahab@MacBook-Pro-de-Rachidi.local>
2023-10-11 22:35:44 +0400

    feat: About

--- src/views/pages/About.jsx
+++ src/views/pages/About.jsx
@@ -1,7 +1,29 @@
+import Banner from '../components/Banner'
+import Collapse from '../components/Collapse'
+
+import image from '/about.jpg?url'
 export default function About() {
   return (
-    <>
-      <h1>About</h1>
-    </>
+    <main className="about">
+      <Banner image={image} />
+      <div className="about__collapse">
+        <Collapse
+          title="Fiabilité"
+          description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+        />
+        <Collapse
+          title="Respect"
+          description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Service"
+          description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Sécurité"
+          description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
+      </div>
+    </main>
   )
 }
